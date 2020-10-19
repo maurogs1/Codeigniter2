ea959a276db3f4a848072ada4d58496689f9442d
Mauro <maurosaravia59@gmail.com>
2020-10-19 02:33:56 -0300

    heroku configuraciones

--- application/config/config.php
+++ application/config/config.php
@@ -22,8 +22,8 @@
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/ci2/';
-//$config['base_url'] = 'https://ci2-app.herokuapp.com/';
+//$config['base_url'] = 'http://localhost/ci2/';
+$config['base_url'] = 'https://ci2-app.herokuapp.com/';
 /*
 |--------------------------------------------------------------------------
 | Index File
